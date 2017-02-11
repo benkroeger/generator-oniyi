b3f48e958df886469e58033d5fabac85659d8aa6
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2017-02-11 13:19:38 +0100

    updates eslintrc.js files

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,9 +1,7 @@
-{
-  "extends": "plugin:ava/recommended",
-  "plugins": [
-    "ava"
-  ],
-  "rules": {
-    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
-  }
-}
+module.exports = {
+  extends: 'plugin:ava/recommended',
+  plugins: [ 'ava' ],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+};
